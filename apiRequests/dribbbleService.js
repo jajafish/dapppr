ea7ad511037b9a0b0612ee2abeaddd9c21728a8d
Jared Fishman <jfish@gigbear.com>
2014-08-15 23:03:25 -0700

    added initial jade template

--- apiRequests/dribbbleService.js
+++ apiRequests/dribbbleService.js
@@ -1,14 +1,14 @@
 var request  = require('request');
 
-request({
-    uri: "http://api.dribbble.com/players/simplebits/shots",
-    method: "GET",
-    timeout: 10000,
-    followRedirect: true,
-    maxRedirects: 10,
-}, function(err, response, body){
+    request({
+        uri: "http://api.dribbble.com/players/simplebits/shots",
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10,
+    }, function(err, response, body){
 
-    var dribbleUserShots = JSON.parse(body);
-    console.log(dribbleUserShots);
+        var dribbleUserShots = JSON.parse(body);
+        console.log(dribbleUserShots);
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+var dribbbleAPI = require('../apiRequests/dribbbleService');
+
+
 exports.sayHello = function (req, res) {
 
-    res.set('Content-Type', 'text/html');
-    res.send('hello jared stephanie seba and jason');
-    console.log('hello hello heloo');
+
+    res.render('signup');
+
+
+
 };
