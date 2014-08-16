29d5cb0d32099b69a8c6475dc0fb042b04299550
Jared Fishman <jfish@gigbear.com>
2014-08-15 22:56:58 -0700

    hello team

---
+++ app.js
@@ -0,0 +1,29 @@
+var express                 = require('express'),
+    routes                  = require("./routes"),
+    app                     = express(),
+    server                  = require("http").createServer(app),
+    bodyParser              = require("body-parser"),
+    http                    = require('http'),
+    request                 = require('request');
+
+
+app.set('port', 3000);
+server.listen(3000);
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser());
+app.use(bodyParser.json());
+
+app.use(function (req, res, next){
+    console.log(req.body);
+    next();
+});
+
+
+app.get('/', routes.sayHello);
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {layout: true});
+
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+exports.sayHello = function (req, res) {
+
+    res.set('Content-Type', 'text/html');
+    res.send('hello jared stephanie seba and jason');
+    console.log('hello hello heloo');
+};
