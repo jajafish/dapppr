39316ffbe66aa79ef1832062fe918f1446e98817
Jared Fishman <jfish@gigbear.com>
2014-08-16 17:10:59 -0700

    connecting to robo mongo inside one function to say hello

--- app.js
+++ app.js
@@ -5,16 +5,13 @@ var express                 = require('express'),
     bodyParser              = require("body-parser"),
     http                    = require('http'),
     mongo                   = require('mongodb'),
-    db = new mongo.Db('mydb', new mongo.Server("127.0.0.1", 27017, {}), {}),
+    // db                      = require('./lib/db'),
     config                  = require('config'),
     utils                   = require('./lib/utils'),
     request                 = require('request'),
     fs                      = require('fs'),
     util                    = require('util'),
     mime                    = require('mime');
-
-
-
 
 app.set('port', 3000);
 server.listen(3000);
@@ -39,52 +36,30 @@ app.use(bodyParser.json());
 // SHOW USER PRODUCTS PAGE
 // app.get('/:userId', routes.showUserProductsPage);
 
-app.get('/testkang', function (req, res) {
-   
-    request.get({url: 'https://d13yacurqjgara.cloudfront.net/users/44490/screenshots/1248634/kangaroorunappgameicon.png', encoding: 'binary'}, function (err, response, body) {
-        if (err) return;
-
-        var binaryKangPNG = body;
-        console.log(binaryKangPNG);
-
-        db.open(function(err, db) {
-            db.collection('users', function(err, collection) {
-                doc = {
-                    "kangPNGFile" : binaryKangPNG
-                };
-                collection.insert(doc, function() {
-                    db.close();
-                });
-            });
-        });
-
-    });
-
-});
-
-app.get('/showAlienImage', function (req, res){
 
 
-    db.open(function(err, db){
-        db.collection.find(), function(err, docs){
-            console.log(docs);
+app.get('/hello', function (req, res){
+
+var uri = 'mongodb://johnnyJones:pingpong1@ds063449.mongolab.com:63449/dapppr';
+mongo.MongoClient.connect(uri, {server: {auto_reconnect: true}}, function (err, db){
+
+
+    db.collection('phrases', function(err, collection) {
+        doc = {
+            "hello" : "you"
         };
-
+        collection.insert(doc, function() {
+            db.close();
+        });
     });
 
 
 
-    // db.open(function(err, db){
-    //     db.collection('users', function(err, collection){
-    //         collection.find({"_id" : "53efe09a531bd30f23afb430"}, function(err, docs) {
-    //             console.log(docs).toArray();
-    //         });
-    //     });
-    // });
-
-    // // console.log(alienImage);
+});
 
 });
+
+
 
 
 app.set('views', __dirname + '/views');
